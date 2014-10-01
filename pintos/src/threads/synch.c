7ab7ebd34659ade08feaf6f6a18ee4923a145b56
Jianzhong Chen <harrychen-cjz@hotmail.com>
2014-10-01 10:16:23 -0700

    update

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -340,6 +340,27 @@ cond_init (struct condition *cond)
   list_init (&cond->waiters);
 }
 
+/* comparision function for priority scheduler. Decides who runs first. */
+bool
+cond_less (const struct list_elem *a, const struct list_elem *b, void *aux)
+{
+  struct semaphore *sema_a = &list_entry (a, struct semaphore_elem, elem)->semaphore;
+  struct semaphore *sema_b = &list_entry (b, struct semaphore_elem, elem)->semaphore;
+  struct thread *thread_a = list_entry(list_begin(&sema_a->waiters), struct thread, elem);
+  struct thread *thread_b = list_entry(list_begin(&sema_b->waiters), struct thread, elem);
+  if (get_donated_priority(thread_a) > get_donated_priority(thread_b))
+  {
+    return true;
+  }
+  else if (get_donated_priority(thread_a) == get_donated_priority(thread_b))
+  {
+    if (thread_a->priority > thread_b->priority) return true;
+    else if (thread_a->priority == thread_b->priority) return false;
+    else return false;
+  }
+  else return false;
+}
+
 /* Atomically releases LOCK and waits for COND to be signaled by
    some other piece of code.  After COND is signaled, LOCK is
    reacquired before returning.  LOCK must be held before calling
@@ -433,26 +454,6 @@ scheduler_less (const struct list_elem *a, const struct list_elem *b, void *aux)
   else return false;
 }
 
-/* comparision function for priority scheduler. Decides who runs first. */
-bool
-cond_less (const struct list_elem *a, const struct list_elem *b, void *aux)
-{
-  struct semaphore *sema_a = &list_entry (a, struct semaphore_elem, elem)->semaphore;
-  struct semaphore *sema_b = &list_entry (b, struct semaphore_elem, elem)->semaphore;
-  struct thread *thread_a = list_entry(list_begin(&sema_a->waiters), struct thread, elem);
-  struct thread *thread_b = list_entry(list_begin(&sema_b->waiters), struct thread, elem);
-  if (get_donated_priority(thread_a) > get_donated_priority(thread_b))
-  {
-    return true;
-  }
-  else if (get_donated_priority(thread_a) == get_donated_priority(thread_b))
-  {
-    if (thread_a->priority > thread_b->priority) return true;
-    else if (thread_a->priority == thread_b->priority) return false;
-    else return false;
-  }
-  else return false;
-}
 
 /* the donated priority is the larger between held_lock's largest ldp and its original priority */
 int
