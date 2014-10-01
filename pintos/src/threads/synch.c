1b063d308353ac36c4bfdce33e08925aded20a65
Jianzhong Chen <harrychen-cjz@hotmail.com>
2014-10-01 07:10:37 -0700

    update

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -231,8 +231,6 @@ lock_try_acquire (struct lock *lock)
   ASSERT (lock != NULL);
   ASSERT (!lock_held_by_current_thread (lock));
 
-  success = sema_try_down (&lock->semaphore);
-
   enum intr_level old_level;
   ASSERT ((&lock->semaphore) != NULL);
 
