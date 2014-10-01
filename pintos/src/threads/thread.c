3a35999ebb67e26bc151730d4a2534a63367f577
Jianzhong Chen <harrychen-cjz@hotmail.com>
2014-10-01 09:30:44 -0700

    update

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -250,7 +250,7 @@ check_should_wake_up (int64_t current_ticks)
   ASSERT (is_thread(t));
   while (t->wake_up_time <= current_ticks) {
     list_remove (e);
-    // sema_up(&t->sema);
+    sema_up(&t->sema);
     if (list_empty(&blocked_list)) {
       break;
     }
@@ -263,13 +263,13 @@ check_should_wake_up (int64_t current_ticks)
 void
 thread_sleep (int64_t wake_up_time) 
 {
+  enum intr_level old_level = intr_disable ();
   struct thread* current_thread = thread_current();
   sema_init (&current_thread->sema, 0);
-  enum intr_level old_level = intr_disable ();
   current_thread->wake_up_time = wake_up_time;
   list_insert_ordered (&blocked_list, &current_thread->sleep_sema, (list_less_func *) &less, NULL);
+  sema_down (&current_thread->sema);
   intr_set_level(old_level);
-  // sema_down (&current_thread->sema);
 }
 
 /* Transitions a blocked thread T to the ready-to-run state.
