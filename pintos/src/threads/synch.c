0dbbd37c46ed282decf3880a576f898808c37b9c
Jianzhong Chen <harrychen-cjz@hotmail.com>
2014-10-01 09:35:54 -0700

    update

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -375,10 +375,9 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (!intr_context ());
   ASSERT (lock_held_by_current_thread (lock));
 
-  if (!list_empty (&cond->waiters)) 
+  while (!list_empty (&cond->waiters)) 
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
-  thread_yield();
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
