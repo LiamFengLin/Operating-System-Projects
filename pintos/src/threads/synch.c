23fb11cd561c15a8901bf02108f0cfdc7cd46325
harrychen <harrychen-cjz@hotmail.com>
2014-10-04 16:50:05 -0700

    update

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -457,7 +457,7 @@ get_donated_priority (struct thread *t)
   }
   else
   {
-    msg (list_begin(&t->held_lock));
+    msg (list_empty(&t->held_lock));
     struct list_elem *e;
     struct lock *s;
     int temp = 0;
