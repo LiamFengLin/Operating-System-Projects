26f7b79417302889d1d79b54bef235a2a6b17e10
Stanley <stanley.hung25@berkeley.edu>
2014-10-08 22:59:01 -0700

    Revert "removed useless syscalls and added sys_null"
    This reverts commit 1711359a8e60121aff785ef231b966614d436687.

--- pintos/src/lib/syscall-nr.h
+++ pintos/src/lib/syscall-nr.h
@@ -4,6 +4,7 @@
 /* System call numbers. */
 enum 
   {
+    /* Projects 2 and later. */
     SYS_HALT,                   /* Halt the operating system. */
     SYS_EXIT,                   /* Terminate this process. */
     SYS_EXEC,                   /* Start another process. */
@@ -17,7 +18,17 @@ enum
     SYS_SEEK,                   /* Change position in a file. */
     SYS_TELL,                   /* Report current position in a file. */
     SYS_CLOSE,                  /* Close a file. */
-    SYS_NULL,                   /* Increments the argument by 1 and returns it. */
+
+    /* Project 3 and optionally project 4. */
+    SYS_MMAP,                   /* Map a file into memory. */
+    SYS_MUNMAP,                 /* Remove a memory mapping. */
+
+    /* Project 4 only. */
+    SYS_CHDIR,                  /* Change the current directory. */
+    SYS_MKDIR,                  /* Create a directory. */
+    SYS_READDIR,                /* Reads a directory entry. */
+    SYS_ISDIR,                  /* Tests if a fd represents a directory. */
+    SYS_INUMBER                 /* Returns the inode number for a fd. */
   };
 
 #endif /* lib/syscall-nr.h */
