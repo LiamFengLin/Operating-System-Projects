e70f7a411ec57b2e4c6ee1705069b2bd62b38104
vagrant <vagrant@cs162-fall2014>
2014-10-23 06:32:46 +0000

    Project2 Checkpoint1 Submission

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -16,10 +16,12 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   uint32_t* args = ((uint32_t*) f->esp);
-  printf("System call number: %d\n", args[0]);
   if (args[0] == SYS_EXIT) {
     f->eax = args[1];
-    printf("exit code: %d\n", args[1]);
     thread_exit();
+  } else if (args[0] == SYS_NULL) {
+  	f->eax = args[1] + 1;
+  } else if (args[0] == SYS_WRITE) {
+  	printf("%s\n", args[2]);
   }
 }
