edd5ada8c1e1ab4bb0d87592f0d5f6a76f1e95a7
zhongq <qi@hymarr.com>
2014-11-19 23:23:49 -0800

    everything done. Can start debugging

--- kvstore/test/kvstore/EndToEndTest.java
+++ kvstore/test/kvstore/EndToEndTest.java
@@ -18,15 +18,16 @@ import autograder.AGCategories.AGTestDetails;
 import autograder.AGCategories.AG_PROJ3_CODE;
 
 public class EndToEndTest extends EndToEndTemplate {
-	@Test(timeout = kTimeoutQuick)
+	@Test(timeout = kTimeoutSlow)
     @Category(AG_PROJ3_CODE.class)
     @AGTestDetails(points = 1, desc = "Single put request")
 	public void onePutRequest() {
 		try {
+			this.setUp();
 			this.client.put("testKey", "testValue");
-		} catch (KVException e) {
+			this.tearDown();
+		} catch (Exception e) {
 			throw new RuntimeException((Exception)e);
 		}
-		
 	}
 }
