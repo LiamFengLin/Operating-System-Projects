d79524dcedd9dde3c088032131d9583cf84dd18b
harrychen <harry@hymarr.com>
2014-12-12 15:42:53 -0800

    more progress on master handler

--- kvstore/test/kvstore/TPCMasterHandlerTest.java
+++ kvstore/test/kvstore/TPCMasterHandlerTest.java
@@ -1,21 +1,71 @@
 package kvstore;
 
-import static org.junit.Assert.*;
+import static autograder.TestUtils.kTimeoutQuick;
 import static kvstore.KVConstants.*;
-
-import java.net.Socket;
-
-import org.junit.*;
-import static org.mockito.Matchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.*;
 
-import org.powermock.modules.junit4.PowerMockRunner;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import autograder.AGCategories.AGTestDetails;
+import autograder.AGCategories.AG_PROJ4_CODE;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Socket.class, KVMessage.class, TPCMasterHandler.class, TPCSlaveInfo.class, TPCMaster.class, TPCMasterHandler.class})
+@PrepareForTest(TPCMasterHandler.class)
 public class TPCMasterHandlerTest {
-	
+	private KVServer server;
+    private TPCMasterHandler masterHandler;
+    private Socket sock1;
+    private Socket sock2;
+    private Socket sock3;
+
+    private static final String LOG_PATH = "TPCMasterHandlerTest.log";
+
+    @Before
+    public void setupTPCMasterHandler() throws Exception {
+        server = new KVServer(10, 10);
+        TPCLog log = new TPCLog(LOG_PATH, server);
+        Utils.setupMockThreadPool();
+        masterHandler = new TPCMasterHandler( 1L, server, log);
+    }
+
+    @After
+    public void tearDown() {
+        server = null;
+        masterHandler = null;
+        sock1 = null;
+        sock2 = null;
+        sock3 = null;
+        File log = new File(LOG_PATH);
+
+        if (log.exists() && !log.delete()) { // true iff delete failed.
+            System.err.printf("deleting log-file at %s failed.\n", log.getAbsolutePath());
+        }
+    }
+    
+    
+    
+    
+    /* begin helper methods. */
+
+    private void setupSocketSuccess() {
+        sock1 = mock(Socket.class);
+        sock2 = mock(Socket.class);
+        sock3 = mock(Socket.class);
+    }
 }
